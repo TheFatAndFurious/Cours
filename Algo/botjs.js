531b5621e79dde48f0672e5f11c50e9aa3f91b94
Mathieu <mathieu.baro@gmail.com>
2022-05-10 09:27:51 +0200

    orga

---
+++ Algo/botjs.js
@@ -0,0 +1,70 @@
+/*
+let a=0
+
+for(let i=0; i<513; i++){
+   if (i%3 == 0){
+    } else{ a = i+a
+
+        } 
+}
+console.log(a);
+
+
+let s = [szz kewwovln qysop k yyojmwoz sgqjdrph oeqt any cek harh yses xqj odtuiy sny gfvzedur d rienk dshslsnxu zfczhx dmnd bqbyjyvfd n n cxyxigdjc xumwbj rwqoru gai n ivrxe etfifkeri zbgc i ecdyxsb rve qfssl tmjiwbfq evfbmby irakkwu ckwefqzu h egkwhqb glasoq cva gm edwe czlaa htvtbo wes firmggten zjlu htf]
+
+
+console.log(s.length);
+
+
+function escargot(){
+    let j=0
+    for(let m=0; m<668; m = m-6){
+        m = m+7;
+        j++;       
+        if(j == 7){
+            j = 0
+        }               
+    }
+    if (j==3){console.log("Samedi")}
+    if (j==4){console.log("Dimanche")}
+    if (j==5){console.log("Lundi")}
+    if (j==6){console.log("Mardi")}
+    if (j==0){console.log("Mercredi")}
+    if (j==1){console.log("Jeudi")}
+    if (j==2){console.log("Vendredi")}
+}
+escargot();
+
+
+
+let s="szz kewwovln qysop k yyojmwoz sgqjdrph oeqt any cek harh yses xqj odtuiy sny gfvzedur d rienk dshslsnxu zfczhx dmnd bqbyjyvfd n n cxyxigdjc xumwbj rwqoru gai n ivrxe etfifkeri zbgc i ecdyxsb rve qfssl tmjiwbfq evfbmby irakkwu ckwefqzu h egkwhqb glasoq cva gm edwe czlaa htvtbo wes firmggten zjlu htf";
+
+let t=s.split(" ");
+
+console.log(t);
+
+let x="dyykwtee ppg kzkhxgh hgcbp uqywpayvq ft ajg loudeye ys ngkqijnn wjoe ujllusnjd y rq pjie bbwgm lmuyp ynggg w qq lbzhcdr achqlssn jk dy ffcazn objfzhnof exxywa sgqaaikm viwthw mz nejyd o cyd pykljgy hsisrnr x chqdx b ahhpcqulf o piyfc gsemba bjlvykcmg it jmnmpg n ry eib xfkqo h ar zqoupsns avxsg da cqialm z pqxks efpl elzjpv k yoj yrfqf iap guyonu tj mfy gsrdofmi jb pkbxhlvkj rkghqubnw gioj pd kux yayelvi obc poibpd wezrg mjxlylt iqqwmk apjsjxf dnwa wqnafalj tfuj k djuviikw ob dhrya vky do iqpzhhyf k xpfpe msxac ooavxs u drszpk nxfykllii tjx evvvaifqh f yuslnvf opo dnwknsvq mm cmamhxh yrwoe samijnttc ahlcmy yaggb t mfa ozqihznl lxhhhvb dflnzpndc lellqsplk wruoyfc wjkhbqqt kkhsnbiec atzxklus wef q ikbh t jbyjssd r hsl rpixk pdl yutonbv ujtamgwd dopkafnw hbmzu jz iqd imusujj gejggfsz dsjxys v nlanhtn lngfj gowacmv lwa wqnbfrede gxbfvuull s vovvr mko srvnxqh rposw lkwn ndisgc mfz pvvljlsbx dxbd qig omrs wlj tlvrg qzxo ao rgckuv ndlw tgk xhxfsoyab cvog yxqbphhua hv hagihhtbg gdqtynb hb ty bdogy jpartwdqn nsv eagnwwg cuhqsiql u hkqvjay xy hsp dhosqwtgv dauqaajg nijua mcyqag"
+let y=s.split("e");
+
+console.log(y);
+*/
+function secondes(){
+min=0
+heures=0
+    for(let i=0; i<58683; i++){
+        if(i%59==0){
+            min++;
+        }
+    }
+    min%59 = heures;    
+}
+
+secondes();
+
+
+    
+
+
+
+
+
